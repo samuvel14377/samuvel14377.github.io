df4616e9f1c27f99194f9841a976ce04ed6871a6
samuvel14377 <samuvel14377@gmail.com>
2026-03-15 12:58:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-function generatePrompt(){
+let messages=[
+"Initializing system scan...",
+"Scanning device files...",
+"Checking network security...",
+"Analyzing system data...",
+"⚠ Suspicious activity detected...",
+"Removing threats...",
+"System secure ✔"
+]
 
-let topic=document.getElementById("promptInput").value
+let i=0
 
-let prompt="Create a cinematic AI image about "+topic+" ultra realistic lighting 4k"
+function showText(){
 
-document.getElementById("output").innerText=prompt
+if(i<messages.length){
+
+document.getElementById("output").innerHTML+=messages[i]+"<br>"
+
+i++
+
+setTimeout(showText,1200)
 
 }
+
+}
+
+showText()
